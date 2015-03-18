48b9305b07c9cc5b181699f7d5f6b9a847fa1598
Trcx <trcx528@gmail.com>
2015-03-18 12:47:57 -0500

    added rf api

--- src/main/java/com/trcx/ita_tech/Client/Proxy.java
+++ src/main/java/com/trcx/ita_tech/Client/Proxy.java
@@ -4,5 +4,8 @@ package com.trcx.ita_tech.Client;
  * Created by Trcx on 3/18/2015.
  */
 public class Proxy extends com.trcx.ita_tech.Common.Proxy {
-
+    @Override
+    public void registerRenderers() {
+        super.registerRenderers();
+    }
 }
--- src/main/java/com/trcx/ita_tech/Common/Proxy.java
+++ src/main/java/com/trcx/ita_tech/Common/Proxy.java
@@ -4,4 +4,5 @@ package com.trcx.ita_tech.Common;
  * Created by Trcx on 3/18/2015.
  */
 public class Proxy {
+    public void registerRenderers(){}
 }
